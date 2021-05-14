48f37dde587e9d708541b40a1a3e9df75878fdce
Salim Ahmedabadi <ahmedabadi.salim@gmail.com>
2021-05-14 12:35:33 -0500

    feat: Started working on Junit exercise

--- src/main/java/Student.java
+++ src/main/java/Student.java
@@ -1,9 +1,10 @@
-import java.util.ArrayList;
+
 
 public class Student {
+Student(String name)
     // returns the student's id
     public long getId(){
-
+        return long Student;
         }
 
     // returns the student's name
