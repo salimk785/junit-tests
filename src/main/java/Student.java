de25c6a9e5a97d24f2ca2b91f38ec8231a77aaf7
Salim Ahmedabadi <ahmedabadi.salim@gmail.com>
2021-05-14 12:00:55 -0500

    feat: Started working on Junit Tests Exercise

---
+++ src/main/java/Student.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+
+public class Student {
+    // returns the student's id
+    public long getId(){
+
+        }
+
+    // returns the student's name
+    public String getName(){
+
+        }
+
+    // adds the given grade to the grades list
+    public void addGrade(int grade){
+
+        }
+
+    // returns the list of grades
+    public ArrayList<Integer> getGrades(){
+
+        }
+
+    // returns the average of the students grades
+    public double getGradeAverage(){
+
+        }
+
+}
